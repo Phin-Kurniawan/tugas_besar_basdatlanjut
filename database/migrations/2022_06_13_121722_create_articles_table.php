25740f14af35ab752e5c440fcfaa957c95c2f778
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-06-14 00:11:25 +0700

    Articles migration and seeder

---
+++ database/migrations/2022_06_13_121722_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('pic', 500);
+            $table->string('header', 50);
+            $table->string('subtitle', 200);
+            $table->string('content_path', 200);
+            $table->string('featured', 4);  // true / blank
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
