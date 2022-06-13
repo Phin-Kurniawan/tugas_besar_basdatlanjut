1f0258e7f987314a63dcf0f75547b7803cf0e14d
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-06-13 18:42:30 +0700

    Update dependencies

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,10 +15,12 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+	{{-- Data Tables --}}
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.12.1/datatables.min.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.12.1/datatables.min.js"></script>
 
-    <script type="text/javascript" charset="utf8"
-            src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+	{{-- Bootstrap 5 --}}
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     @yield('addCss')
 </head>
@@ -41,11 +43,9 @@
 
 <!-- REQUIRED SCRIPTS -->
 
+{{-- Bootstrap 5 --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-<!-- jQuery -->
-<script src="{{asset('js/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('js/adminlte.min.js')}}"></script>
 <!-- Sweetalert -->
@@ -54,7 +54,6 @@
 @yield('addJavascript')
 <script>
     $(document).ready(function () {
-        confirm("??")
         $("table").DataTable();
     });
 </script>
