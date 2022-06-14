4db6b30f0e991ad1c0755a3f36e56299de49dc62
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-06-14 07:13:22 +0700

    Change dashboard route to home

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('dashboard')}}" class="brand-link">
+    <a href="{{route('home')}}" class="brand-link">
         <img src="{{asset('img/pet-central-logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">Pet Central</span>
@@ -26,7 +26,7 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="{{route('dashboard')}}" class="nav-link">
+                    <a href="{{route('home')}}" class="nav-link">
                         <i class="nav-icon fa fa-home"></i>
                         <p>Home</p>
                     </a>
