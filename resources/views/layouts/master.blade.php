89ad66520027c325d19f8644adfbddd81d3291bf
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-06-14 09:12:16 +0700

    Add custom styling for carousels

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,6 +22,18 @@
 	{{-- Bootstrap 5 --}}
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
+    <style>
+        .carouselimg {
+            filter: brightness(60%);
+            border-radius: 20px;
+        }
+
+        .cardwidth {
+            width: 320px;
+            margin: 10px
+        }
+    </style>
+
     @yield('addCss')
 </head>
 
